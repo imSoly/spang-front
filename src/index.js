59de8a63da0eb825107845239bf866a86f508b64
imSoly <imsoly@gmail.com>
2021-11-09 00:49:53 +0900

    remove basename at BrouserRouter

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/spang-front">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
